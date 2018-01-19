4fe72ff35d246e7561b33cc43f09640eaa6d1ce3
spacive <haluskajuraj0@gmail.com>
2018-01-19 15:20:46 +0100

    dtw type1 implemented

---
+++ dtw/dtw1.m
@@ -0,0 +1,61 @@
+function [d] = dtw1(A, B)
+
+% dimension of vectors in [A] and [B] (must be the same)
+dim = size(A ,1);
+
+% compute euclidean distances between [dim]-dimensional
+% vectors in array [A] and array [B]
+localDist = zeros(length(B), length(A));
+for a = 1:length(A)
+    for b = 1:length(B)
+        for i = 1:dim
+            square = (A(i, a) - B(i, b))^2;
+            localDist(b, a) = localDist(b, a) + square;
+            % normally, the square root would be taken 
+            % but for purpose of dtw it's not needed
+        end
+    end
+end
+
+% compute global distances based on local conditions
+globalDist = zeros(length(B), length(A));
+for a = 1:length(A)
+    for b = 1:length(B)
+        if a == 1 && b == 1
+            globalDist(b, a) = localDist(b, a);
+        else
+            % first row represents validity of condition
+            % second row represents actual value
+            valids = [];
+
+            % condition 1
+            i = b;
+            j = a - 1;
+            if i > 0 && j > 0
+                valids = [valids globalDist(i, j) + localDist(b, a)];
+            end
+            
+            % condition 2
+            i = b - 1;
+            j = a - 1;
+            if i > 0 && j > 0
+                valids = [valids globalDist(i, j) + 2 * localDist(b, a)];
+            end
+            
+            % condition 3
+            i = b - 1;
+            j = a;
+            if i > 0 && j > 0
+                valids = [valids globalDist(i, j) + localDist(b, a)];
+            end
+            
+            % find and save minimum value to globalDist
+            globalDist(b, a) = min(valids);
+        end
+    end
+end
+
+% resulting normalised distance
+d = globalDist(length(B), length(A)) / (length(B) + length(A));
+
+end
---
+++ dtw/test.m
@@ -0,0 +1,6 @@
+clear all;
+
+A = [1 2 -1 0;0 3 2 0];
+B = [1 2 1;1 3 0];
+
+distance = dtw1(A, B)
