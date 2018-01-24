f0588e046f56904c2220b5e7766929240cba907e
spacive <haluskajuraj0@gmail.com>
2018-01-24 15:56:47 +0100

    mfcc first version

--- mfcc/mfcc_test.m
+++ mfcc/mfcc_test.m
@@ -1,3 +1,26 @@
 % test file for mfcc
 
-melFilterBank = mel_fb(100, 10); 
+bandsCount = 40;
+dctsCount = 12;
+
+[y, Fs] = audioread('records/zosit.wav');
+
+% subsignal
+sub = y(2000:3000);
+sub = sub .* hamming(length(sub))';
+
+% spectrum
+spect = abs(fft(sub));
+spect = spect(1:ceil(length(spect) / 2));
+
+% log of spectrum
+spectLog = log10(spect);
+
+% apply filter bank
+melFilterBank = mel_fb(length(spectLog), bandsCount);
+filtered = spectLog * melFilterBank;
+
+% perform discrete cosine transform
+dcts = dct(filtered);
+
+mfcc = dcts(1:dctsCount);
