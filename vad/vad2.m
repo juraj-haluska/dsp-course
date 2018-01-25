b9d699cefaa94f28167ae32f434f635287196e4b
spacive <haluskajuraj0@gmail.com>
2018-01-25 18:34:34 +0100

    new version of voice activity detection which can find multiple words - need to fix duration filter

---
+++ vad/vad2.m
@@ -0,0 +1,104 @@
+function [out] = vad2(m, Fs)
+% Voice Activity Detection
+
+% constants
+zerosPeakTh = 0.3;
+zerosPeakDiffTh = 0.05;
+energyMagTh = 0.01;
+durationMs = 350;
+
+% energy and number of zero-crossings for each frame
+e = sum(m .* m) / 2;
+z = zero_crossing(m);
+
+% normalize zeros and energy to range between 0 - 1
+maxEnergy = max(e);
+maxZeros = max(z);
+e = e ./ maxEnergy;
+z = z./ maxZeros;
+
+% mean of zero crossings
+zerosMean = sum(z) / length(z);
+variance = (z - zerosMean).^2 ./ length(z);
+
+% create low pass filter
+b = poly([-1, 0, 0]);
+p1 = 0.93;
+p2 = 0.90*exp(1j*pi/20);
+p3 = conj(p2);
+a = poly([p1, p2 p3]);
+
+% low pass filter zeros
+filtered = filter(b, a, variance);
+filtered = filtered ./ max(filtered);
+
+% find peaks in filtered zeros
+[pks,locs] = findpeaks(filtered);
+
+% filter zeros peaks by zerosPeakTh
+zerosPeaks = [];
+for i=1:length(pks)
+    if pks(i) >= zerosPeakTh
+        zerosPeaks = [zerosPeaks locs(i)];
+    end
+end
+
+% variable representing valid zero-regions
+validZeros = zeros(1, length(filtered));
+for i=1:length(zerosPeaks)
+    peakValue = filtered(zerosPeaks(i));
+    % left side to the current peak
+    index = zerosPeaks(i);
+    while index >= 1 && (peakValue - filtered(index)) <= zerosPeakDiffTh
+        validZeros(index) = 1;
+        index = index - 1;
+    end
+    % right side to the current peak
+    index = zerosPeaks(i);
+    while index <= length(filtered) && (peakValue - filtered(index)) <= zerosPeakDiffTh
+        validZeros(index) = 1;
+        index = index + 1;
+    end
+end
+plot(filtered); hold on; plot(validZeros, 'r');
+
+% filter energy according to treshold - energyMagTh
+for i=1:length(e)
+   if e(i) >= energyMagTh
+       e(i) = 1;
+   else
+       e(i) = 0;
+   end
+end
+
+% combine valid regions from energy and zeros
+validRegions = e + validZeros;
+
+figure; plot(validRegions);
+
+% filter valid regions by duration filter 
+frameMs = Fs / size(m ,1);
+offset = ceil(durationMs / frameMs);
+for i=2:length(validRegions)
+    if validRegions(i - 1) > 0 && validRegions(i + offset) > 0
+        validRegions(i) = 1;
+    end
+end
+
+figure; plot(validRegions);
+
+% create output matrix
+count = 0;
+for i=2:length(validRegions)
+    % transition from invalid to valid
+    if validRegions(i - 1) == 0 && validRegions(i) > 0
+        count = count + 1;
+        out(1, count) = i;
+    end
+    % transition from valid to invalid
+    if validRegions(i - 1) > 0 && validRegions(i) == 0
+       out(2, count) = i; 
+    end
+end
+
+end
---
+++ vad/vad2_test.m
@@ -0,0 +1,25 @@
+[y, Fs] = audioread('viac_slov.wav');
+
+close all;
+
+% constants
+overlap = floor(Fs * 0.01);
+frameSize = floor(Fs * 0.03);
+
+% pre-emphasis
+y = filter([1 -0.9], 1, y);
+
+% split signal into frames with overlap
+m = split(y, frameSize, overlap);
+m = m .* hamming(frameSize);
+
+% find valid regions in frames
+out = vad2(m, Fs);
+
+% save isolated records
+for i=1:length(out)
+    firstSample = frameSize * out(1, i) - (out(1, i) - 1) * overlap;
+    lastSample = frameSize * out(2, i) - (out(2, i) - 1) * overlap;
+    valid = y(firstSample:lastSample);
+    audiowrite(strcat(num2str(i),'.wav'), valid, Fs);
+end
